80bd090fd39e32986fd865b7710f40414d7ba310
RubensJr21 <junior.correa2001@gmail.com>
2022-07-26 12:02:16 -0300

    Improves the code that generates dummy data from the table

--- src/App/dados.ts
+++ src/App/dados.ts
@@ -1,5 +1,11 @@
 import { ITable, ItemBody, ItemHead } from '../components/Table/types'
 
+const numberBetween = (floorNumber: number, topNumber: number) => {
+    const numbers = Array.from({length: (topNumber + 1) - floorNumber}, (f, g) => g + floorNumber);
+    const pos = Math.floor(Math.random() * numbers.length)
+    return numbers[pos];
+}
+
 // gera número entre 0 e 4, somando mais 1 fica entre 1 e 5
 const qtdColunas = Math.floor(Math.random() * 4) + 1
 // gera número entre 0 e 15, somando mais 1 fica entre 1 e 16
@@ -8,8 +14,10 @@ const qtdRegistros = Math.floor(Math.random() * 15) + 1
 const _head: Array<ItemHead> = []
 const _body: Array<ItemBody> = []
 
-for (let index = 1; index < qtdColunas + 1; index++) {
-    _head.push(`Coluna ${index}`)
+const colunaDeContato = Math.floor(Math.random() * qtdColunas)
+
+for (let index = 0; index < qtdColunas + 0; index++) {
+    _head.push(index === colunaDeContato ? "número" : `Coluna ${index + 1}`)
 }
 
 const descriptorBase = {
@@ -21,10 +29,47 @@ const descriptorBase = {
 for (let indexR = 0; indexR < qtdRegistros; indexR++) {
     _body.push({} as ItemBody)
     for (let indexC = 0; indexC < qtdColunas; indexC++) {
+        const getPhoneNumber = (needToBeValid: boolean) => {
+            const digits = numberBetween(8,9)
+            const verifyNumber = (numberPhone: string, digits: number) => {
+                return numberPhone.length === digits
+            }
+            const fixNumber = (numberPhone: string, digits: number) => {
+                return `${numberPhone}${numberPhone}`.slice(0, digits)
+            }
+            // 10 ** digits (10^digits) representa a quantidade de cada decimais que o número terá
+            // 10^2 = 100; 10^3 = 1000,..., 10^8 = 100.000.000, 10^9 = 1.000.000.000
+            // Logo o número, possivelmente, terá a quantidade de dígitos esperados
+            // Para os casos em que o número fique muito pequeno, ele será tratado como erro sendo usado
+            // para mostrar ao usuário que aquele valor não corresponde a um número "válido"
+            // Obs: desconsidera-se o numero começar com o 9 ou 8, apenas a quantidade de dígitos é validado
+            const numberTmp = `${Math.floor(Math.random() * (10 ** digits))}`
+            const numberFull = false ? numberTmp : (!verifyNumber(numberTmp, digits)) ? numberTmp : fixNumber(numberTmp, digits)
+            console.log({
+                numberFull
+            })
+            // Se o numero tiver 9 dígitos quer dizer que antes do traço ele terá 5 números
+            // Se não quer dizer que o número passado, neste teste, será 8, por tanto, terá
+            // 4 número antes do traço
+            const separator = digits === 9 ? 5 : 4
+            return `+55 27 ${numberFull.slice(0, separator)}-${numberFull.slice(separator, numberFull.length)}` 
+        }
+
+        const getValue = (index: number) => {
+            if(index !== colunaDeContato){
+                return `Item[${indexR + 1},${indexC + 1}]`
+            } else {
+                // Sorteia se o registro será um registro válido ou não
+                // Usado para aplicar a classe danger
+                const isValid = Math.floor(Math.random() * 10) === 5
+                console.log({isValid})
+                return getPhoneNumber(isValid)
+            }
+        }
 
         Object.defineProperty(_body[indexR], `${_head[indexC]}`, {
             ...descriptorBase,
-            value: `Item[${indexR + 1},${indexC + 1}]`
+            value: getValue(indexC)
         })
     }
 }
--- src/components/Table/types.ts
+++ src/components/Table/types.ts
@@ -3,32 +3,32 @@ import { Dispatch, SetStateAction } from "react"
 export type ItemHead = string
 
 export interface ItemBody {
-	[key: string]: string
+	[key: string]: string,
 }
 
 // example
 const Table: ITable = {
-	head: ["coluna 1", "coluna 2", "coluna 3"],
+	head: ["coluna 1", "coluna 2", "numero"],
 	body: [
 		{
 			"coluna 1": "registro[1,1]",
 			"coluna 2": "registro[1,2]",
-			"coluna 3": "registro[1,3]",
+			"numero": "registro[1,3]",
 		},
 		{
 			"coluna 1": "registro[2,1]",
 			"coluna 2": "registro[2,2]",
-			"coluna 3": "registro[2,3]",
+			"numero": "registro[2,3]",
 		},
 		{
 			"coluna 1": "registro[3,1]",
 			"coluna 2": "registro[3,2]",
-			"coluna 3": "registro[3,3]",
+			"numero": "registro[3,3]",
 		},
 		{
 			"coluna 1": "registro[4,1]",
 			"coluna 2": "registro[4,2]",
-			"coluna 3": "registro[4,3]",
+			"numero": "registro[4,3]",
 		}
 	]
 }
