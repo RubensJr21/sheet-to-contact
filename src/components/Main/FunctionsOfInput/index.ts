f1fcd3d643ee4e8f047a27d06e4fb5a32c3e1f95
RubensJr21 <junior.correa2001@gmail.com>
2022-08-09 19:10:23 -0300

    Adds new verification if verifies if has commas in the data

--- src/components/Main/FunctionsOfInput/index.ts
+++ src/components/Main/FunctionsOfInput/index.ts
@@ -4,19 +4,47 @@ export const verifyDataCsv = (csv: string): {isValid: boolean, lines: Array<Arra
     /*
       Verificar o csv com base no tamanho das linha
       Passo:
-        1. Dar split na quebra linha
-        2. Para cada linha gerada dar split no separador usado
-        3. Comparar se todos as listas tem o mesmo tamanho
-        4. Se estiver tudo certo passar para tela de Table
+        1.  Verificar se existe vírgula nos dados e tratar
+        1.1 Caso exista vírgula nos dados ele irá trocar por <comma> antes da mudança de separador
+        1.2 Muda separador
+        1.3 Volta com as vírgulas dos dados trocando de <comma> para ',' após da mudança de separador
+        1.4 Limpa a string CSV
+        1.5 Dar split na quebra linha
+        2.  Para cada linha gerada dar split no separador usado
+        3.  Comparar se todos as listas tem o mesmo tamanho
+        4.  Se estiver tudo certo passar para tela de Table
     */
-    // -1. Garante que o separador do arquivo será uma vírgula, logo em seguida limpa os caracteres indesejados
-    csv = csv.replaceAll(";", ",").replaceAll("\r", "");
     //  0. Inicializa variável de validação
     let isCsvValid = true;
 
-    // 1. e 2.
+    // 1.
+    const stringInCSV  = /(".*?")/g
+    // Link de ajuda: Obtendo todos 'matchs' das expressões procuradas (matchAll)
+    const termos = Array.from(csv.matchAll(stringInCSV), m => m[0]);
+
+    // 1.1
+    if(termos.length > 0){
+        const newTermos = termos.map((termo: string) => termo.replaceAll(",", "<comma>"))
+    
+        termos.forEach((termo: string, index: number) => {
+            csv = csv.replace(termo, newTermos[index])
+        })
+    }
+    
+    // 1.2
+    csv = csv.replaceAll(",", ";")
+
+    // 1.3
+    if(termos.length > 0){
+        csv = csv.replaceAll("<comma>", ",")
+    }
+
+    // 1.4
+    csv = csv.replaceAll("\r", "").replaceAll('"',"");
+
+    // 1.5
     const lines = csv.split("\n").map((linha: string) => {
-        return linha.split(",");
+        return linha.split(";");
     });
 
     // 3.
