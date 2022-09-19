30e5788dc6f557682057a3e3379269b9f3b86ac5
RubensJr21 <junior.correa2001@gmail.com>
2022-09-19 18:20:09 -0300

    Implements automated phone number formatting

--- src/components/Modals/Format/index.tsx
+++ src/components/Modals/Format/index.tsx
@@ -1,4 +1,5 @@
 import React, { useContext, ChangeEvent, MouseEvent } from "react";
+import { numberPhoneColumns } from "../../../configs";
 import { FormatContext } from "../../../Contexts/Format";
 import { TableContext } from "../../../Contexts/Table";
 import { ITableContext } from "../../Table/types";
@@ -35,17 +36,86 @@ const Format = () => {
         })
     }
 
+    const formatNumber = (keyOut: string, number: string) => {
+        var value = number;
+        number = number.replaceAll(/[^0-9]/g, "");
+        var reg: RegExp
+        var match: Array<string> = []
+        // var match: RegExpMatchArray | null
+        switch (number.length) {
+            /*
+            98765432      -  8
+            987654321     -  9
+            27987654321    - 10
+            279987654321   - 11
+            5527987654321  - 12
+            55279987654321 - 13
+            */
+            case 8:
+                reg = /^([0-9]{4})([0-9]{4})$/g
+                match = reg.exec(number) ?? [];
+                match = match.slice(1, match.length).reverse()
+                value = `+55 27 ${match[1]}-${match[0]}`
+                break;
+            case 9:
+                reg = /^([0-9]{5})([0-9]{4})$/g
+                match = reg.exec(number) ?? [];
+                match = match.slice(1, match.length).reverse()
+                value = `+55 27 ${match[1]}-${match[0]}`
+                break;
+            case 10:
+                reg = /^([0-9]{2})([0-9]{4})([0-9]{4})$/g
+                match = reg.exec(number) ?? [];
+                match = match.slice(1, match.length).reverse()
+                value = `+55 ${match[2]} ${match[1]}-${match[0]}`
+                break;
+            case 11:
+                reg = /^([0-9]{2})([0-9]{5})([0-9]{4})$/g
+                match = reg.exec(number) ?? [];
+                match = match.slice(1, match.length).reverse()
+                value = `+55 ${match[2]} ${match[1]}-${match[0]}`
+                break;
+            case 12:
+                reg = /^(55)([0-9]{2})([0-9]{4})([0-9]{4})$/g
+                match = reg.exec(number) ?? [];
+                match = match.slice(1, match.length).reverse()
+                value = `+${match[3]} ${match[2]} ${match[1]}-${match[0]}`
+                break;
+            case 13:
+                reg = /^(55)([0-9]{2})([0-9]{5})([0-9]{4})$/g
+                match = reg.exec(number) ?? [];
+                match = match.slice(1, match.length).reverse()
+                value = `+${match[3]} ${match[2]} ${match[1]}-${match[0]}`
+                break;
+            default:
+                break;
+        }
+        setDataFormat((oldState) => {
+            var newState = { ...oldState }
+            if(Object.keys(newState.registro).includes(keyOut)){
+                newState["registro"][keyOut] = value
+            }
+            return newState
+        })
+    }
+
 	const renderInputs = () => {
         return Object.keys(dataFormat.registro).map((key: string, index: number) => {
+            // console.log({key, registro: dataFormat.registro[key], contains_key: numberPhoneColumns.includes(key)});
+            const columnName: string = dataFormat.colunas[index];
+            const number: string = dataFormat.registro[key];
             return (
                 // Tentar colocar botão que formata automaticamente os campos
                 // Cujo as estruturas já sejam conhecidas. Ex.: Celular e CPF
                 <div key={index} className="input-group mb-3">
-                    <span className="input-group-text" id="basic-addon1">{dataFormat.colunas[index]}</span>
+                    <span className="input-group-text" id="basic-addon1">{columnName}</span>
                     <input type="text" className="form-control"
-                        value={dataFormat.registro[key]}
+                        value={number}
                         onChange={(e) => onChangeValue(e, key)}
-                        aria-label={dataFormat.registro[key]} aria-describedby="basic-addon1" />
+                        aria-label={number} aria-describedby="basic-addon1" />
+                    {numberPhoneColumns.includes(key) ? 
+                    <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick={(e) => formatNumber(key, number)}>Format</button>
+                    : null}
                 </div>
             )
         })
